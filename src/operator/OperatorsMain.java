216bae1e724f743e5b8d28f6a4e0225bdad00df8
test <test>
2020-02-01 17:18:13 +0300

    classwork

--- src/operator/OperatorsMain.java
+++ src/operator/OperatorsMain.java
@@ -8,5 +8,16 @@ public class OperatorsMain {
         System.out.println(a);
         a += 1;
         System.out.println(a);
+
+        int sum = 0;
+        int i = 0;
+        while (i < 20){
+            if (i % 2 == 0){
+                sum += i;
+            }
+            i++;
+        }
+
+        System.out.println("sum = " + sum);
     }
 }
