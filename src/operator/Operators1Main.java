078e7c7191397876f5a434af78059b08990fd77c
nyatta <nyattacat@gmail.com>
2020-02-01 16:18:26 +0300

    classwork

---
+++ src/operator/Operators1Main.java
@@ -0,0 +1,15 @@
+package operator;
+
+public class Operators1Main {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println( a == 10);
+        System.out.println( a == 11);
+
+        if(a == 10){
+            System.out.println("a = 10");
+        }else{
+            System.out.println("A != 10");
+        }
+    }
+}
---
+++ src/operator/OperatorsMain.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class OperatorsMain {
+    public static void main(String[] args) {
+        int a = 0;
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println(a);
+        a += 1;
+        System.out.println(a);
+    }
+}
